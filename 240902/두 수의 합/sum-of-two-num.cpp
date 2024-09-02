bc4742e703e086878bd87da3fdb6aeaca9712fbb
이승준 <105282117+sengjun0624@users.noreply.github.com>
2024-09-02 16:02:22 +0900

    Title: 두 수의 합, Time: 105ms, Memory: 5MB, Status: Passed - CodeTree

--- 240902/두 수의 합/sum-of-two-num.cpp
+++ 240902/두 수의 합/sum-of-two-num.cpp
@@ -6,17 +6,10 @@ using namespace std;
 #define ll  long long
 map<ll, ll> m;
 
-ll factorial(ll n) {
-    ll ans = 1;
-    for (ll i = 1; i <= n; i++) {
-        ans *= i;
-    }
-    return ans;
-}
 
 ll permutation(ll n, ll k) {
     if (n == 0)return 0;
-    return factorial(n) / (factorial(n - k) * factorial(k));
+    return n * (n - 1) / 2;
 }
 
 int main() {
@@ -27,12 +20,14 @@ int main() {
     for (ll i = 0; i < n; i++) {
         ll num;
         cin >> num;
+
         if (m.find(num) == m.end()) {
             m[num] = 1;
         } else m[num]++;
     }
     //0으로 바꾸면
     ll ans = 0;
+
     for (auto pair: m) {
         // k-* 의 *이 first
         ll First = pair.first;
@@ -41,7 +36,7 @@ int main() {
         // k에서 뺀값 = *에서 X를 더하면 K가 되는수
         ll Second = k - First;
         // 같은 수를 더해서 k가 될때
-        if (Second > INT_MAX || Second < INT_MIN) continue;
+
         if (Second == First) {
             //k는 4, n=2
             ans += permutation(freq, 2);
