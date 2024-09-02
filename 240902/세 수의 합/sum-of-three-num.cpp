f83dde943b460d54d73f79e3245596052857d4cd
이승준 <105282117+sengjun0624@users.noreply.github.com>
2024-09-02 16:52:10 +0900

    Title: 세 수의 합, Time: 1095ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 240902/세 수의 합/sum-of-three-num.cpp
+++ 240902/세 수의 합/sum-of-three-num.cpp
@@ -1,30 +1,35 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+
 using namespace std;
 
-
 int main() {
-    // 여기에 코드를 작성해주세요.
     int n,k;
-    cin>>n>>k;
-    vector<int>v(n);
+    cin >> n >> k;
 
-    for(int i=0;i<n;i++){
-        cin>>v[i];
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
     }
-    vector<int>check(n,0);
-    check[0]=1;check[1]=1;check[2]=1;
+
+    // 선택할 3개의 원소에 대응하는 마스크를 생성합니다.
+    vector<bool> selectMask(n, false);
+    fill(selectMask.end() - 3, selectMask.end(), true);  // 뒤에서부터 3개의 원소를 선택
+
+    // 배열을 정렬하여 초기 상태 설정
+    sort(arr.begin(), arr.end());
     int ans=0;
-    do{
+    do {
+        // 현재 조합 출력
         int sum=0;
-        for(int i=0;i<n;i++){
-            if(check[i]){
-                sum+=v[i];
+        for (int i = 0; i < n; i++) {
+            if (selectMask[i]) {
+                sum+=arr[i];
             }
         }
         if(sum==k)ans++;
-    }while(next_permutation(v.begin(),v.end()));
-    cout<<ans/3;
+    } while (next_permutation(selectMask.begin(), selectMask.end()));
+    cout<<ans;
     return 0;
 }
